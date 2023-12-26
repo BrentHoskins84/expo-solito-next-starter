233613bc11906ba53f9a766ec78560fff3a68ade
Brent Hoskins <brenthoskins84@gmail.com>
2023-12-25 18:43:02 -0800

    getting expo router drawers working

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Provider } from 'app/provider'
 import { Stack } from 'expo-router'
+import { Drawer } from 'expo-router/drawer'
 
 export default function Root() {
   return (
     <Provider>
-      <Stack />
+      {/* <Stack /> */}
+      <Drawer />
     </Provider>
   )
 }
